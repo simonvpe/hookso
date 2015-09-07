40dee481de4a0265b9b50a07b0e0e2d906578791
Simon Pettersson <simon.v.pettersson@gmail.com>
2015-09-07 21:57:28 +0200

    Fixed the interface such that one can configure the dispatch using static consts within the implementation classes.

--- hook.cpp
+++ hook.cpp
@@ -8,6 +8,11 @@ using namespace hookso;
 // Hook implementation for malloc. Just prints to cout when pre/post hooks
 // are called.
 static struct Hook_malloc : public Hook<Hook_malloc, void*(size_t)> {
+  // Config
+  static const bool enable_prehook = true;
+  static const bool enable_posthook = false;
+  static const bool enable_implementation = false;
+
   inline void prehook(size_t) const {
     cout << "malloc pre-hook called" << endl;
   }
@@ -16,16 +21,18 @@ static struct Hook_malloc : public Hook<Hook_malloc, void*(size_t)> {
   }
   static constexpr const char *function_name() { return "malloc"; }
 } malloc_hook;
-template<> struct enable_prehook<Hook_malloc>       { static const bool value = true;  };
-template<> struct enable_posthook<Hook_malloc>      { static const bool value = true;  };
-template<> struct enable_func_override<Hook_malloc> { static const bool value = false; };
 void *malloc(size_t size) { return malloc_hook(size); }
 
 // Override rand to provide a sequence of numbers
-//HOOK_BEGIN(Hook_rand, int())
 static struct Hook_rand : public Hook<Hook_rand, int()> {
+
+  // Config
+  static const bool enable_prehook = false;
+  static const bool enable_posthook = false;
+  static const bool enable_implementation = true;
+
   int count;
-  inline int func_override() {
+  inline int implementation() {
     int actual = fn();
     cout << "rand ovveride called. Substituting random number "
          << actual << " for " << count << endl;
@@ -34,11 +41,7 @@ static struct Hook_rand : public Hook<Hook_rand, int()> {
   static const char *function_name() { return "rand"; }
 
   Hook_rand() : count(0) {}
-//HOOK_END(Hook_rand)
 } rand_hook;
-template<> struct enable_prehook<Hook_rand>       { static const bool value = false; };
-template<> struct enable_posthook<Hook_rand>      { static const bool value = false; };
-template<> struct enable_func_override<Hook_rand> { static const bool value = true;  };
 int rand(void) { return rand_hook(); }
 
 // TODO: Re-implement a c++ class member
--- hook.h
+++ hook.h
@@ -7,22 +7,33 @@ extern "C" {
 #  include <dlfcn.h>
 }
 
-// Make specializations of these traits to override default
-// behaviour.
-template<typename Impl_Type>
-struct enable_prehook {
-  static const bool value = true;
-};
+// Check which features are enabled. If the class defines a type called
+// enable_##X it picks that value, otherwise it picks default_value
+#define MAKE_ENABLE_CHECK(X, default_value)				\
+  template<typename Impl_Type>						\
+  struct enable_##X {							\
+									\
+  template<typename U>							\
+  struct has_##X {							\
+  typedef char is_match;						\
+  typedef long no_match;	 					\
+  template<typename C> static is_match test( decltype(&C::enable_##X) );\
+  template<typename C> static no_match test(...);			\
+  enum { value = sizeof(test<U>(0)) == sizeof(char) };			\
+  };									\
+									\
+  template<typename U, bool select> struct get_##X;			\
+  template<typename U> struct get_##X<U,false> { enum { value = default_value }; }; \
+  template<typename U> struct get_##X<U,true>  { enum { value = U::enable_##X }; }; \
+									\
+  static const bool value = get_##X<Impl_Type, has_##X<Impl_Type>::value>::value; \
+  };									\
 
-template<typename Impl_Type>
-struct enable_posthook {
-  static const bool value = true;
-};
-
-template<typename Impl_Type>
-struct enable_func_override {
-  static const bool value = true;
-};
+// We want to be able to check for prehook, posthook, and implementation in order
+// to pick whether to dispatch function calls during compile time.
+MAKE_ENABLE_CHECK(prehook, true)
+MAKE_ENABLE_CHECK(posthook, true)
+MAKE_ENABLE_CHECK(implementation, true)
 
 namespace hookso {
   using namespace std;
@@ -40,56 +51,69 @@ namespace hookso {
     using Function_T = Return_Type (*)(Args...);
     Function_T fn;
 
-  public:
+    // Constructor stores a function pointer to the actual function we are overriding
     Hook() {
       this->fn = reinterpret_cast<Function_T>(dlsym(RTLD_NEXT, Impl_Type::function_name())); 
     }
     virtual ~Hook() {}
+
+    // Disable copying
     Hook(const Hook &) = delete;
     Hook &operator=(const Hook &) = delete;
 
+  public:
+    
+    // Call procedure, in the function override you must call this function with the actual
+    // arguments.
     Return_Type operator()(Args & ... args) const {
-      prehook(args...);
-      Return_Type ret = func_override(args...);
-      posthook(ret);
+      _prehook(args...);
+      Return_Type ret = _impl(args...);
+      _posthook(ret);
       return ret;
     }
 
     // *************************************************************************************
     // void prehook(Args & ... args)
+    // *************************************************************************************
     template<typename T = Impl_Type>
       inline typename enable_if< !enable_prehook<T>::value, void >::type
-      prehook(Args & ... args) const {
-      // Do nothing
+      _prehook(Args & ... args) const {
+      // Do nothing if enable_prehook != true
     }
     template<typename T = Impl_Type>
       inline typename enable_if< enable_prehook<T>::value, void >::type
-      prehook(Args & ... args) const {
+      _prehook(Args & ... args) const {
+      // Call prehook if enable_prehook == true
       reinterpret_cast<const Impl_Type*>(this)->prehook(args...);
     }
     // *************************************************************************************
     // void posthook(Return_Type &ret)
+    // *************************************************************************************
     template<typename T = Impl_Type>
       inline typename enable_if< !enable_posthook<T>::value, void >::type
-      posthook(Return_Type &ret) const {
-      // Do nothing
+      _posthook(Return_Type &ret) const {
+      // Do nothing if enable_posthook != true
     }
     template<typename T = Impl_Type>
       inline typename enable_if< enable_posthook<T>::value, void >::type
-      posthook(Return_Type &ret) const {
+      _posthook(Return_Type &ret) const {
+      // Call posthook if enable_posthook == true
       reinterpret_cast<const Impl_Type*>(this)->posthook(ret);
     }
     // *************************************************************************************
     // Return_Type func_override(Args...)
+    // *************************************************************************************
     template<typename T = Impl_Type>
-      inline typename enable_if< !enable_func_override<T>::value, Return_Type >::type
-      func_override(Args & ... args) const {
+      inline typename enable_if< !enable_implementation<T>::value, Return_Type >::type
+      _impl(Args & ... args) const {
+      // Call actual function if enable_implementation != true
       return const_cast<This_T*>(this)->fn(args...); 
     }
     template<typename T = Impl_Type>
-      inline typename enable_if< enable_func_override<T>::value, Return_Type >::type
-      func_override(Args & ... args) const {
-      return reinterpret_cast<Impl_Type*>(const_cast<This_T*>(this))->func_override(args...);
+      inline typename enable_if< enable_implementation<T>::value, Return_Type >::type
+      _impl(Args & ... args) const {
+      // Call user implementation if enable_implementation == true
+      return reinterpret_cast<Impl_Type*>(const_cast<This_T*>(this))->implementation(args...);
     }
     // *************************************************************************************
   };
